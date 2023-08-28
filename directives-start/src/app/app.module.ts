ea276b5966e387b39b9fed3fe13da56a9aa9e95d
Raisa Toscano <daisa1690@gmail.com>
2023-08-27 22:05:16 -0600

    Examples of ngclass, ngif, ngfor, and directives

---
+++ directives-start/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    BasicHighLightDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
